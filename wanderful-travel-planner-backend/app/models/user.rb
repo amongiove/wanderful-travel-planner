411489219612c8cf69082c08cac398b55e29e207
amongiove <amandamongiove@gmail.com>
2021-09-28 10:07:58 -0500

    WP-3 generate userTrips model, table, and designate appropriate associations

--- wanderful-travel-planner-backend/app/models/user.rb
+++ wanderful-travel-planner-backend/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+
+    has_many :user_trips
+    has_many :trips, through: :user_trips 
 end
--- wanderful-travel-planner-backend/db/schema.rb
+++ wanderful-travel-planner-backend/db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_192538) do
+ActiveRecord::Schema.define(version: 2021_09_28_145209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "trips", force: :cascade do |t|
+    t.string "location"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_trips", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "trip_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["trip_id"], name: "index_user_trips_on_trip_id"
+    t.index ["user_id"], name: "index_user_trips_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +40,6 @@ ActiveRecord::Schema.define(version: 2021_09_27_192538) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "user_trips", "trips"
+  add_foreign_key "user_trips", "users"
 end
