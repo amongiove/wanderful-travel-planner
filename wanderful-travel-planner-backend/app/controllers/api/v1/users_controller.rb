6c29e2e96f1f6c22e0bb696a6f87adc73ce768ea
amongiove <amandamongiove@gmail.com>
2021-09-28 18:20:27 -0500

    WP-4 generate user serializer and update user index and show methods to utilize serializer

--- wanderful-travel-planner-backend/app/controllers/api/v1/users_controller.rb
+++ wanderful-travel-planner-backend/app/controllers/api/v1/users_controller.rb
@@ -3,26 +3,26 @@ class Api::V1::UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
-
-    render json: @users
+    users = User.all
+    users_json = UserSerializer.new(users).serialized_json
+    render json: users_json
   end
 
   # GET /users/1
   def show
-    render json: @user
+    render json: UserSerializer.new(@user).serialized_json
   end
 
   # POST /users
-  def create
-    @user = User.new(user_params)
+  # def create
+  #   @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  #   if @user.save
+  #     render json: @user, status: :created, location: @user
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # # PATCH/PUT /users/1
   # def update
