d9a026f73a9878691219819c82062a75fdb892b3
amongiove <amandamongiove@gmail.com>
2021-09-29 15:31:24 -0500

    WP-6 install redux and set up store

--- wanderful-travel-planner-frontend/src/index.js
+++ wanderful-travel-planner-frontend/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+
+const users = () => []
+const reducer = combineReducers({users})
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
