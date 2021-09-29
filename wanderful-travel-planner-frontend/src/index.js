569d72a2099830ff03240ace0e327b18235c81aa
amongiove <amandamongiove@gmail.com>
2021-09-29 15:53:19 -0500

    WP-6 continue store set up and create base userReducer file

--- wanderful-travel-planner-frontend/src/index.js
+++ wanderful-travel-planner-frontend/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 
-const users = () => []
-const reducer = combineReducers({users})
+import App from './App';
+import usersReducer from './reducers/usersReducer';
+
+const rootReducer = combineReducers({
+  users: usersReducer
+})
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
