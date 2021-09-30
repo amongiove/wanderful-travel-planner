bd4932eb8ec20be0b2b333bd193cdcba1a0bb9bd
amongiove <amandamongiove@gmail.com>
2021-09-30 10:34:29 -0500

    WP-7 generate loginform reducer and action files, separate store code to own store.js file

--- wanderful-travel-planner-frontend/src/index.js
+++ wanderful-travel-planner-frontend/src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
-import thunk from 'redux-thunk'
+import store from './store.js'
 import {Provider} from 'react-redux'
 
-import App from './App';
-import usersReducer from './reducers/usersReducer';
-import currentUserReducer from './reducers/currentUserReducer';
 
-const rootReducer = combineReducers({
-  users: usersReducer,
-  current_user: currentUserReducer
-})
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ wanderful-travel-planner-frontend/src/store.js
@@ -0,0 +1,17 @@
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import usersReducer from './reducers/usersReducer';
+import currentUserReducer from './reducers/currentUserReducer';
+import loginFormReducer from './reducers/loginFormReducer';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+    users: usersReducer,
+    currentUser: currentUserReducer,
+    loginForm: loginFormReducer
+  })
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
