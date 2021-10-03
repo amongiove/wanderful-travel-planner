dde50973939f6a4721f23246127d658af47d3fe6
amongiove <amandamongiove@gmail.com>
2021-10-03 12:43:48 -0500

    add user model valiations for name, email, and password

--- wanderful-travel-planner-backend/app/models/user.rb
+++ wanderful-travel-planner-backend/app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
     has_many :accomodations
     has_many :messages, foreign_key: :sender_id, class_name: "Message"
 
-    validates :name, presence: true
-    validates :email, uniqueness: true
+    validates :name, :email, :password, presence: true
+    validates :email, uniqueness: { case_sensitive: false }
     validates :password, length: { in: 6..20 }
 
 end
