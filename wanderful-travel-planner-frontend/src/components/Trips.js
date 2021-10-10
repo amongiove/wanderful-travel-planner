0e3fcc5992a870535ce440f6c81d75b57b1add90
amongiove <amandamongiove@gmail.com>
2021-10-10 12:33:00 -0500

    move add trip display to trips container

--- wanderful-travel-planner-frontend/src/components/Trips.js
+++ wanderful-travel-planner-frontend/src/components/Trips.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import TripCard from './TripCard.js'
-import TripForm from './TripForm.js'
 import Row from 'react-bootstrap/Row';
 
 const Trips = (props) => {
@@ -11,9 +10,6 @@ const Trips = (props) => {
 
     return (
         <div>
-        <Row xs={1} md={-1} className="justify-content-end" style={{padding: "10px"}}>
-            attach trip form
-        </Row>
         <Row xs={1} md={3} className="g-4">
             {tripCards} 
         </Row>
--- wanderful-travel-planner-frontend/src/containers/TripsContainer.js
+++ wanderful-travel-planner-frontend/src/containers/TripsContainer.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import Container from 'react-bootstrap/Container'
 import { getTrips } from '../actions/trips.js';
 import Trips from '../components/Trips';
+import TripForm from '../components/TripForm.js'
+import Row from 'react-bootstrap/Row';
+
 
 class TripsContainer extends React.Component {
 
@@ -18,6 +21,9 @@ class TripsContainer extends React.Component {
         <div>
         {/* TODO: do i want to have users name here? if so need to add current user to state to get the name attribute */}
           <h1>Trips</h1>
+          <Row xs={1} md={-1} className="justify-content-end" style={{padding: "10px"}}>
+            attach trip form
+          </Row>
           <Trips trips={trips}/>
         </div>
 
