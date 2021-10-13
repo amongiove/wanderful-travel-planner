f34acaaba96a54b77fb5c969d0ccba7c315627e0
amongiove <amandamongiove@gmail.com>
2021-10-12 22:26:43 -0500

    add image and image def to trips serializer

--- wanderful-travel-planner-backend/app/serializers/trip_serializer.rb
+++ wanderful-travel-planner-backend/app/serializers/trip_serializer.rb
@@ -1,4 +1,9 @@
 class TripSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :location, :start_date, :end_date
+  attributes :name, :location, :start_date, :end_date, :image
+
+  def image
+    rails_blob_path(object.image, only_path: true) if object.image.attached?
+  end
+
 end
