93773bc660cd8e20c9bab3764a72c17887732f13
amongiove <amandamongiove@gmail.com>
2021-10-13 13:10:54 -0500

    implement countdown to trip on trip card

--- wanderful-travel-planner-frontend/src/components/trips/TripCard.js
+++ wanderful-travel-planner-frontend/src/components/trips/TripCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import moment from 'moment';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
 
 
 const TripCard = ({trip}) => {
+
+    const start_date = trip.attributes.start_date;
+    const now = moment();
+    const past = (moment(start_date, "YYYY/MM/DD").isBefore(moment()));
+    const countdown = (now.to(start_date, true))
 
     return (
         // TODO: are we adding images? make card clickable or remove hover css
@@ -16,7 +22,7 @@ const TripCard = ({trip}) => {
                     <Card.Title>{trip.attributes.name}</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">{trip.attributes.location}</Card.Subtitle>
                     <Card.Text>
-                    countdown to trip 
+                       { past === true ? "0 days to go!" : `${countdown} to go!` }
                     </Card.Text>
                     {/* TODO: change link to make name of trip */}
                     <Link to={`/trips/${trip.id}`}>View Trip</Link>
