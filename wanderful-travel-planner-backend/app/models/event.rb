5e5f96bce9eb9f9b3610f9ecc0dedaa5589313ed
amongiove <amandamongiove@gmail.com>
2021-10-19 14:32:17 -0500

    implement event validations

--- wanderful-travel-planner-backend/app/models/event.rb
+++ wanderful-travel-planner-backend/app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :trip
+
+  validates :even_name, presence: true
+  validates :location, presence: true
+  validates :event_date_time, presence: true
 end
