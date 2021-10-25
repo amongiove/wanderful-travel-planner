e5fe2d45b962bd045b3d0b48ab5bc1fa970e97df
amongiove <amandamongiove@gmail.com>
2021-10-25 12:56:20 -0500

    generate flights reducer

---
+++ wanderful-travel-planner-frontend/src/reducers/flightsReducer.js
@@ -0,0 +1,37 @@
+const initialState = {
+    flights: []
+}
+
+const flightsReducer = (state = initialState, action) => {
+    switch (action.type){
+        case "SET_FLIGHTS":
+            return {
+                ...state,
+                flights: action.flights
+            }
+        case 'FLIGHT':
+            return {
+                ...state,
+                flights: [...state.flights, action.flight ]
+            }
+        case 'EDIT_FLIGHT':
+            const editedFlights = state.flights.map(flight => {
+                return (flight.id === action.flight.id) ? action.flight : flight
+            });
+            return { 
+                ...state, 
+                flights: editedFlights
+            }            
+        case "DELETE_FLIGHT":
+            return {
+                flights: [
+                    ...state.flights.slice(0, action.flightId),
+                    ...state.flights.slice(action.flightId + 1)
+                ]
+            }
+        default: 
+            return state
+    }
+}
+
+export default flightsReducer;
