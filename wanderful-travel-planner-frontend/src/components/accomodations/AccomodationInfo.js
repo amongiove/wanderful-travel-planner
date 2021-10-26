d39a205bc050dd0096521b57fdf9233aa2ba688b
amongiove <amandamongiove@gmail.com>
2021-10-26 10:50:16 -0500

    create accomodaions container and begin to format

---
+++ wanderful-travel-planner-frontend/src/components/accomodations/AccomodationInfo.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import moment from 'moment';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { GrFormNextLink, GrTrash } from 'react-icons/gr';
+import styled from 'styled-components';
+// import EditFlightForm from './EditFlightForm.js';
+
+const Styles = styled.div`
+
+    .location, .checkin, .checkout {
+        text-align: left;
+    }
+
+    .edit {
+        float: right;
+        margin-right: 10px;
+    }
+
+    .delete {
+        float:right;
+    }
+`;
+
+const AccomodationInfo = ({accomodation, trip, onDelete, onEdit}) => {
+
+    const handleDelete = async (event) => {
+        event.preventDefault();
+        const result = await onDelete(accomodation.id);
+        if (result && !result.error) {
+            return alert('Accomodation Deleted')
+            // TODO: reload page without switching tab??? 
+        }
+    }
+
+    return (
+        <Styles>
+            <ListGroup.Item> 
+                <Row>
+                    <Col className="location" md={4}>
+                        {accomodation.attributes.location}
+                    </Col>
+                    <Col className="checkin" md={3}>
+                        Check in: {moment(accomodation.attributes.start_date_time).format("MMM Do, YYYY h:mm A")}  
+                    </Col>
+                    <Col className="checkout" md={3}>
+                        Check out: {moment(accomodation.attributes.end_date_time).format("MMM Do, YYYY h:mm A")}
+                    </Col>
+                    <Col md={2}>
+                        <Button onClick={(event) => (window.confirm('Are you sure you want to delete this accomodation?'))?handleDelete(event) : null} variant="outline-secondary" size="sm" className="delete">
+                            <GrTrash />
+                        </Button>
+                        {/* <EditFlightForm flight={flight} currentTrip={trip} onSubmit={onEdit}/> */}
+                        {/* <Button variant="outline-secondary" size="sm"className="edit">
+                            <GrEdit/>
+                        </Button> */}
+                    </Col>
+                </Row>
+            </ListGroup.Item>
+        </Styles>  
+    )
+}
+
+export default AccomodationInfo
---
+++ wanderful-travel-planner-frontend/src/containers/AccomodationsContainer.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { getAccomodations, deleteAccomodation, createNewAccomodation, editAccomodation } from '../actions/accomodations';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/esm/ListGroup';
+import styled from 'styled-components';
+import AccomodationInfo from '../components/accomodations/AccomodationInfo.js';
+import { connect } from 'react-redux';
+// import NewFlightForm from '../components/flights/NewFlightForm.js';
+
+const Styles = styled.div`
+    .new-accomodation {
+        float: right;
+    }
+
+`;
+
+class AccomodationsContainer extends React.Component {
+
+    componentDidMount = () => {
+        this.props.getAccomodations()
+    }
+        
+    render () {
+        const { accomodations, trip, deleteAccomodation, createNewAccomodation, editAccomodation } = this.props
+
+        const matchedAccomodations = accomodations.filter(accomodation => (parseInt(accomodation.attributes.trip_id) === parseInt(trip.id)))
+        let orderedAccomodations = matchedAccomodations.sort((a,b) => a.attributes.start_date_time > b.attributes.start_date_time ? 1 : -1)
+        const tripAccomodations = orderedAccomodations.map (accomodation => <AccomodationInfo accomodation={accomodation} key={accomodation.id} id={accomodation.id} onDelete={deleteAccomodation} trip={trip} onEdit={editAccomodation} /> )
+
+        return ( 
+            <Styles>
+                <Card className="text-center">
+                    <Card.Body>
+                        <Card.Title>Accomodations</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">{trip.attributes.name}</Card.Subtitle>
+                        <Card.Text>
+                            <ListGroup >
+                                {matchedAccomodations.length > 0 ? tripAccomodations : null}
+                                {/* TODO: no flights msg */}
+                            </ListGroup>
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        new accomodation form
+                        {/* <NewFlightForm onSubmit={createNewFlight} currentTrip={trip} /> */}
+                    </Card.Footer>
+                </Card>
+            </Styles>
+        );
+    }
+};
+
+const mapStateToProps = state => {
+    return {
+        accomodations: state.accomodations.accomodations
+    }
+}
+const mapDispatchToProps = dispatch => {
+    return {
+        getAccomodations: () => dispatch(getAccomodations()),
+        deleteAccomodation: accomodationId => dispatch(deleteAccomodation(accomodationId)),
+        createNewAccomodation: newAccomodation => dispatch(createNewAccomodation(newAccomodation)),
+        editAccomodation: updatedAccomodation => dispatch(editAccomodation(updatedAccomodation))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AccomodationsContainer); 
