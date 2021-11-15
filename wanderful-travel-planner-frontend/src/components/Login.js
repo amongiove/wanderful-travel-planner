306a15eac18c9092487859e5911fd3ce5b2b7049
amongiove <amandamongiove@gmail.com>
2021-11-15 12:35:25 -0600

    require inputs for email and password at login

--- wanderful-travel-planner-frontend/src/components/Login.js
+++ wanderful-travel-planner-frontend/src/components/Login.js
@@ -29,7 +29,7 @@ const Login = ({ loginFormData, updateLoginForm, login, history }) => {
                 <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control name="email" value={loginFormData.email} onChange={handleChange} type="email" placeholder="Enter email" />
+                        <Form.Control name="email" required value={loginFormData.email} onChange={handleChange} type="email" placeholder="Enter email" />
                         <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                         </Form.Text>
@@ -37,7 +37,7 @@ const Login = ({ loginFormData, updateLoginForm, login, history }) => {
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" name="password" value={loginFormData.password} onChange={handleChange}placeholder="Password" />
+                        <Form.Control type="password" required name="password" value={loginFormData.password} onChange={handleChange}placeholder="Password" />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Log In
